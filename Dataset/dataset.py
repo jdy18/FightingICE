76c65f9ac6e916c3d3c6e0301728d9c1b50ead19
MoramiSu <91274335+MoramiSu@users.noreply.github.com>
2023-05-03 17:16:22 +0800

    Add files via upload

---
+++ Dataset/dataset.py
@@ -0,0 +1,32 @@
+import torch
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+import time
+
+class FightingICEDataset(Dataset):
+    def __init__(self, path, sequence_len=32, ROLL_OUT=3600):
+        self.trajectories = torch.load(path)
+        # self.trajectories1 = torch.load(path.replace('_k', '_1'))
+        self.sequence_len = sequence_len
+        truncated_seq_len = torch.clamp(self.trajectories["seq_len"] - sequence_len + 1, 0, ROLL_OUT)  # 舍弃轨迹最后sequence_len个状态，因为以这些状态作为开始的序列无法凑齐长度为sequence_len的序列
+        self.cumsum_seq_len = np.cumsum(np.concatenate((np.array([0]), truncated_seq_len.numpy())))
+
+    def __len__(self):
+        return self.cumsum_seq_len[-1]
+
+    def __getitem__(self, start_idx):
+        eps_idx = np.digitize(start_idx, bins=self.cumsum_seq_len, right=False) - 1  # 当前idx在哪个轨迹中
+        seq_idx = start_idx - self.cumsum_seq_len[eps_idx]  # 当前idx在该轨迹中的索引
+        series_idx = np.linspace(seq_idx, seq_idx + self.sequence_len - 1, num=self.sequence_len, dtype=np.int64)  # 以此idx为首的序列
+        return self.trajectories['actions'][eps_idx, series_idx], self.trajectories['states'][eps_idx, series_idx], self.trajectories['rewards'][eps_idx, series_idx]
+
+if __name__ == '__main__':
+    start_time = time.time()
+    datasets = FightingICEDataset('./Data_random.pth')
+    end_time = time.time()
+    print(end_time-start_time)
+    dataloader = DataLoader(datasets, batch_size=64, shuffle=True)
+    for data in tqdm(dataloader):
+        pass
+
