28391e05574a31ede4ad9eed784d86863db929bd
MoramiSu <91274335+MoramiSu@users.noreply.github.com>
2023-05-15 11:10:07 +0800

    Add files via upload

---
+++ tianshou_cql/test/test.py
@@ -0,0 +1,149 @@
+'''
+test.py
+测试开始前，需要根据自己的actor网络修改load_actor_model（）函数来加载相应的模型
+需要输入的参数：
+actor_path: actor网络的保存位置，建议放在/model文件夹下
+save_path:测试结果保存的位置，建议放在/results文件夹下
+actor_name：atcor网络的名字，决定了如何加载模型
+参考命令：
+python test.py --actor_path ./model/actor.pt --actor_name RecurrentActor --save_path ./results/ppopretrain_vs_MctsAi23i.txt
+'''
+
+import sys
+import argparse
+import torch
+from testagent import TestAgent
+from model import RecurrentActor
+from pyftg.gateway import Gateway
+import logging
+from encoder import SampleEncoder, RawEncoder, FFTEncoder, MelSpecEncoder
+from tianshou_cql.Network import QRDQN
+from tianshou.policy import DiscreteCQLPolicy
+
+STATE_DIM = {
+    1: {
+        'conv1d': 160,
+        'fft': 512,
+        'mel': 2560
+    },
+    4: {
+        'conv1d': 64,
+        'fft': 512,
+        'mel': 1280
+    }
+}
+
+n_frame = 1
+HIDDEN_SIZE = 512
+RECURRENT_LAYERS = 1
+ACTION_NUM = 40
+
+#根据actor_name加载相应的网络， 需要测试其他网络时需要添加新的分支
+def load_actor_model(encoder_name, actor_path, device, actor_name = 'RecurrentActor'):
+    if actor_name == 'RecurrentActor':
+        actor_model = RecurrentActor(STATE_DIM[n_frame][encoder_name], HIDDEN_SIZE, RECURRENT_LAYERS,
+                                     get_sound_encoder(encoder_name, n_frame),
+                                     action_num=ACTION_NUM)
+        actor_state_dict = torch.load(actor_path)
+        actor_model.load_state_dict(actor_state_dict)
+        actor_model.to(device)
+        actor_model.get_init_state(device)  # rnn模型需要初始化状态
+    if actor_name == 'CQL':
+        print('Loading data from '+actor_path)
+        net = QRDQN(hidden_sizes=[512, 512], num_quantiles=50, device=device)
+        optim = torch.optim.Adam(net.parameters(), lr=0.00001)
+        actor_model = DiscreteCQLPolicy(model=net, optim=optim, discount_factor=0.99, num_quantiles=50, estimation_step=1, target_update_freq=500, min_q_weight=50)
+        actor_state_dict = torch.load(actor_path)
+        actor_model.load_state_dict(actor_state_dict)
+    return actor_model
+
+
+def get_sound_encoder(encoder_name, n_frame):
+    encoder = None
+    if encoder_name == 'conv1d':
+        encoder = RawEncoder(frame_skip=n_frame)
+    elif encoder_name == 'fft':
+        encoder = FFTEncoder(frame_skip=n_frame)
+    elif encoder_name == 'mel':
+        encoder = MelSpecEncoder(frame_skip=n_frame)
+    else:
+        encoder = SampleEncoder()
+    return encoder
+
+
+def get_score(self_HP: list, opp_HP: list):
+    total_rounds = len(self_HP)
+    win_rounds = sum([self_HP[i] > opp_HP[i] for i in range(total_rounds)])
+    win_ratio = win_rounds / total_rounds
+    hp_diff = sum([self_HP[i] - opp_HP[i] for i in range(total_rounds)])
+    hp_diff_avg = hp_diff / total_rounds
+    return win_ratio, hp_diff_avg
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--encoder', type=str, choices=['conv1d', 'fft', 'mel'], default='mel',
+                        help='Choose an encoder for the Blind AI')
+    parser.add_argument('--port', type=int, default=50051, help='Port used by DareFightingICE')
+    parser.add_argument('--p2', choices=['Sandbox', 'MctsAi23i'], type=str, default='MctsAi23i', help='The opponent AI')
+    parser.add_argument('--game_num', type=int, default=30, help='Number of games to play')
+    parser.add_argument('--device', type=str, default='cpu', help='device for test')
+    parser.add_argument('--actor_path', type=str, default='./model/actor.pt', help='actor path')  # actor网络路径
+    parser.add_argument('--actor_name', type=str, default='RecurrentActor', help='actor name')  # actor网络名字
+    parser.add_argument('--save_path', type=str, default='./results/random_vs_MctsAi23i.txt', help='save path')  # 结果保存路径
+
+    args = parser.parse_args()
+    characters = ['ZEN']
+
+    device = args.device
+    actor_path = args.actor_path
+    game_num = args.game_num
+    encoder_name = args.encoder
+    actor_name = args.actor_name
+    save_path = args.save_path
+    p2 = args.p2
+
+    # logger config
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(' %(message)s')
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    #file_log 将log输出到文件
+    file_handler = logging.FileHandler(filename=save_path, mode='w')
+    file_handler.setLevel(level=logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    logger.info('Input parameters:')
+    logger.info(' '.join(f'{k}={v}\n' for k, v in vars(args).items()))
+
+    self_HP = []
+    opp_HP = []
+
+    for character in characters:
+        # FFT GRU
+        for _ in range(game_num):
+            # actor模型加载，以blindAI的RNN为例
+            actor_model = load_actor_model(encoder_name=encoder_name, actor_path=actor_path, device=device, actor_name=actor_name)
+            agent = TestAgent(n_frame=n_frame, logger=logger, actor=actor_model, device=device)
+            gateway = Gateway(port=50051)
+            ai_name = 'MelCQL'
+            gateway.register_ai(ai_name, agent)
+            print("Start game")
+            gateway.run_game([character, character], [ai_name, p2], 1)
+            print("After game")
+            sys.stdout.flush()
+            gateway.close()
+
+            results = agent.results
+            for round_result in results:
+                self_HP.append(round_result.remaining_hps[0])
+                opp_HP.append(round_result.remaining_hps[1])
+
+    win_ratio, hp_diff_avg = get_score(self_HP, opp_HP)
+    logger.info("\n win_ratio: %.3f, \n hp_diff_avg %.3f" %(hp_diff_avg, win_ratio))
---
+++ tianshou_cql/test/testagent.py
@@ -0,0 +1,176 @@
+import sys
+sys.path.append('./')
+import numpy as np
+import os
+import torch
+from model import FeedForwardActor, RecurrentActor
+from encoder import RawEncoder, FFTEncoder, MelSpecEncoder, SampleEncoder
+from pyftg.ai_interface import AIInterface
+from pyftg.struct import *
+from tianshou.data import Batch
+import time
+
+STATE_DIM = {
+    1: {
+        'conv1d': 160,
+        'fft': 512,
+        'mel': 2560
+    },
+    4: {
+        'conv1d': 64,
+        'fft': 512,
+        'mel': 1280
+    }
+}
+
+
+class TestAgent(AIInterface):
+    def __init__(self, **kwargs):
+        self.actor = kwargs.get('actor')
+        self.device = kwargs.get('device')
+        self.logger = kwargs.get('logger')
+        self.n_frame = kwargs.get('n_frame')
+        self.trajectories_data = None
+        self.actions = "AIR_A", "AIR_B", "AIR_D_DB_BA", "AIR_D_DB_BB", "AIR_D_DF_FA", "AIR_D_DF_FB", "AIR_DA", "AIR_DB", \
+                   "AIR_F_D_DFA", "AIR_F_D_DFB", "AIR_FA", "AIR_FB", "AIR_UA", "AIR_UB", "BACK_JUMP", "BACK_STEP", \
+                   "CROUCH_A", "CROUCH_B", "CROUCH_FA", "CROUCH_FB", "CROUCH_GUARD", "DASH", "FOR_JUMP", "FORWARD_WALK", \
+                   "JUMP", "NEUTRAL", "STAND_A", "STAND_B", "STAND_D_DB_BA", "STAND_D_DB_BB", "STAND_D_DF_FA", \
+                   "STAND_D_DF_FB", "STAND_D_DF_FC", "STAND_F_D_DFA", "STAND_F_D_DFB", "STAND_FA", "STAND_FB", \
+                   "STAND_GUARD", "THROW_A", "THROW_B"
+        self.audio_data = None
+        self.raw_audio_memory = None
+        self.just_inited = True
+        self.pre_framedata: FrameData = None
+        self.nonDelay: FrameData = None
+        self.round_count = 0
+        self.results = []
+
+
+
+
+    def name(self) -> str:
+        return self.__class__.__name__
+
+    def is_blind(self) -> bool:
+        return False
+
+    def initialize(self, gameData, player):
+        # Initializng the command center, the simulator and some other things
+        self.inputKey = Key()
+        self.frameData = FrameData()
+        self.cc = CommandCenter()
+        self.player = player  # p1 == True, p2 == False
+        self.gameData = gameData
+        self.isGameJustStarted = True
+        return 0
+
+    def close(self):
+        pass
+
+    def get_information(self, frame_data: FrameData, is_control: bool, non_delay: FrameData):
+        # Load the frame data every time getInformation gets called
+        self.frameData = frame_data
+        self.cc.set_frame_data(self.frameData, self.player)
+        # nonDelay = self.frameData
+        self.pre_framedata = self.nonDelay if self.nonDelay is not None else non_delay
+        self.nonDelay = non_delay
+        self.isControl = is_control
+        self.currentFrameNum = self.frameData.current_frame_number  # first frame is 14
+
+    def round_end(self, round_result: RoundResult):
+        self.results.append(round_result)
+        self.logger.info("p1_HP:{}".format(round_result.remaining_hps[0]))
+        self.logger.info("p2_HP:{}".format(round_result.remaining_hps[1]))
+        self.logger.info("elapsed_frame:{}".format(round_result.elapsed_frame))
+        self.just_inited = True
+        self.raw_audio_memory = None
+        self.round_count += 1
+        self.logger.info('Finished {} round'.format(self.round_count))
+
+
+    def input(self):
+        return self.inputKey
+
+    @torch.no_grad()
+    def processing(self):
+        if self.frameData.empty_flag or self.frameData.current_frame_number <= 0:
+            self.isGameJustStarted = True
+            return
+
+        self.inputKey.empty()
+        self.cc.skill_cancel()
+        obs = self.raw_audio_memory
+        if self.just_inited:
+            self.just_inited = False
+            if obs is None:
+                obs = np.zeros((800 * self.n_frame, 2))
+            #self.collect_data_helper.put([obs])
+            terminal = 1
+        elif obs is None:
+            obs = np.zeros((800 * self.n_frame, 2))
+            #self.collect_data_helper.put([obs])
+        else:
+            terminal = 0
+            reward = self.get_reward()
+            #self.collect_data_helper.put([obs, reward, False, None])
+
+        # get action
+        state = torch.tensor(obs, dtype=torch.float32)
+
+        if hasattr(self.actor, 'act'):  # 有act函数用act，否则用forward(原soundagent中的forward函数生成的是分布)
+            action_idx = self.actor.act(state.unsqueeze(0).to(self.device)).float()
+        else:
+            state = state.unsqueeze(0).numpy()
+            batch = Batch({'obs': state, 'info': None})
+            action_idx = self.actor(batch)['logits'].sum(2)
+        action_idx = torch.argmax(action_idx)
+        self.cc.command_call(self.actions[action_idx])
+        self.inputKey = self.cc.get_skill_key()
+
+
+        # action_dist = self.actor(state.unsqueeze(0).to(self.device), terminal=torch.tensor(terminal).float())
+        # action = action_dist.sample()
+        # self.cc.command_call(self.actions[action])
+        # self.inputKey = self.cc.get_skill_key()
+
+        # put to helper
+        #self.collect_data_helper.put_action(action)
+        # if self.rnn:
+        #     self.collect_data_helper.put_actor_hidden_data(self.actor.hidden_cell.squeeze(0).to(self.device))
+        #
+
+
+
+    def get_reward(self):
+        offence_reward = self.pre_framedata.get_character(not self.player).hp - self.nonDelay.get_character(
+            not self.player).hp
+        defence_reward = self.nonDelay.get_character(self.player).hp - self.pre_framedata.get_character(self.player).hp
+        return offence_reward + defence_reward
+
+    def set_last_hp(self):
+        self.last_my_hp = self.nonDelay.get_character(self.player).hp
+        self.last_opp_hp = self.nonDelay.get_character(not self.player).hp
+
+    def get_audio_data(self, audio_data: AudioData):
+        self.audio_data = audio_data
+        # process audio
+        try:
+            byte_data = self.audio_data.raw_data_as_bytes
+            np_array = np.frombuffer(byte_data, dtype=np.float32)
+            raw_audio = np_array.reshape((2, 1024))
+            raw_audio = raw_audio.T
+            raw_audio = raw_audio[:800, :]
+        except Exception as ex:
+            raw_audio = np.zeros((800, 2))
+        if self.raw_audio_memory is None:
+            # self.logger.info('raw_audio_memory none {}'.format(raw_audio.shape))
+            self.raw_audio_memory = raw_audio
+        else:
+            self.raw_audio_memory = np.vstack((raw_audio, self.raw_audio_memory))
+            # self.raw_audio_memory = self.raw_audio_memory[:4, :, :]
+            self.raw_audio_memory = self.raw_audio_memory[:800 * self.n_frame, :]
+
+        # append so that audio memory has the first shape of n_frame
+        increase = (800 * self.n_frame - self.raw_audio_memory.shape[0]) // 800
+        for _ in range(increase):
+            self.raw_audio_memory = np.vstack((np.zeros((800, 2)), self.raw_audio_memory))
